b7fdb6a6328c5c483daf15ae04c85d39ff95073b
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-10-06 10:00:00 +0500

    Add Readme file

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 
-/** A ** prime number ** is a natural number greater than 1 that has no positive divisors other than 1 and itself.In other words, a prime number can only be divided evenly(without a remainder) by 1 and the number itself.
-
-### Examples of Prime Numbers
-  - 2(divisors: 1, 2)
-  - 3(divisors: 1, 3)
-  - 5(divisors: 1, 5)
-  - 7(divisors: 1, 7)
-  - 11(divisors: 1, 11)
-
-### Non - Prime(Composite) Numbers
-Numbers like 4, 6, 8, 9, 10 are ** not ** prime because they have divisors other than 1 and themselves.
-
-### Note
-  - 1 is ** not ** considered a prime number.
-- 2 is the only even prime number.
-**/
 function isPrime(num) {
   if (num <= 1) return false;
   for (let i = 2; i <= Math.sqrt(num); i++) {
